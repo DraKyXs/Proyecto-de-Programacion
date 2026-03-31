a306a3c59546b88bc8dd1a222fcf6dd7408a1d54
Kiasu7w7 <benjaminhernanbarrioslopez@gmail.com>
2026-03-30 23:45:27 -0300

    Cambios benja.java

--- benja.java
+++ benja.java
@@ -1,14 +1,12 @@
-import java.awt.BorderLayout;
 import java.util.*;
 import javax.swing.*;
-import java.awt.Dimension;
-import java.awt.Container;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 
-public class web extends JFrame{
+public class benja extends JFrame{
     JFrame frame;
     
-    public web() {
+    public benja() { //Recordar cambiar nombre de usuario
         frame = new JFrame();
         frame.setTitle("Web");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,6 +18,30 @@ public class web extends JFrame{
         JButton exit = new JButton("Salir");
         JButton min = new JButton("Minimizar");
         JButton max = new JButton("Maximizar");
+        
+        Color azulBonito = new Color(70, 130, 180); //Color nuevo para que se note
+
+        MouseAdapter hoverEfecto = new MouseAdapter() {
+            public void mouseEntered(MouseEvent e) { 
+                JButton b = (JButton)e.getSource();
+                b.setContentAreaFilled(true); // Esto arregla que no cambie el color
+                b.setBackground(azulBonito); 
+                b.setForeground(Color.WHITE); // Texto blanco para que se vea bien
+            }
+            public void mouseExited(MouseEvent e) { 
+                JButton b = (JButton)e.getSource();
+                b.setBackground(null); 
+                b.setForeground(Color.BLACK); // Vuelve a texto negro
+            }
+        };
+
+        // Aplicamos el efecto y quitamos el borde pintado para que el color sea total
+        JButton[] botones = {exit, min, max};
+        for(JButton b : botones) {
+            b.addMouseListener(hoverEfecto);
+            b.setFocusPainted(false);
+            b.setContentAreaFilled(false); // Para que el cambio sea notable
+        }
 
         toolbar.add(exit);
         toolbar.add(min);
@@ -43,7 +65,7 @@ public class web extends JFrame{
     }
 
     public static void main(final String[] args) {
-        new web();
+        new benja();
     }
 
 }
