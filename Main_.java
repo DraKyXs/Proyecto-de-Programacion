f830e88ed10e16176360580a884830348b435fcf
DraKyXs <lcorrea23@alumnos.utalca.cl>
2026-04-28 18:08:47 -0400

    Codigo prueba conexion TCP

--- Main_.java
+++ Main_.java
@@ -22,7 +22,7 @@ catch (IOException | InterruptedException e) {
             		e.printStackTrace();
         	}
 
-}
+
 
 
 /*
@@ -71,5 +71,51 @@ class Main {
 }
 */
 
+/*
 
+// PRUEBA CONEXION TCP
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+class Main {
+	public static void main(String[] args) {
+		String url = "https://jsonplaceholder.typicode.com/todos/1";
+		String respuesta = "";
+		try {
+			respuesta = peticionHttpGet(url);
+			System.out.println("La respuesta es:\n" + respuesta);
+		} catch (Exception e) {
+			// Manejar excepción
+			e.printStackTrace();
+		}
+	}
+
+	public static String peticionHttpGet(String urlParaVisitar) throws Exception {
+		// Esto es lo que vamos a devolver
+		StringBuilder resultado = new StringBuilder();
+		// Crear un objeto de tipo URL
+		URL url = new URL(urlParaVisitar);
+
+		// Abrir la conexión e indicar que será de tipo GET
+		HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
+		conexion.setRequestMethod("GET");
+		// Búferes para leer
+		BufferedReader rd = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+		String linea;
+		// Mientras el BufferedReader se pueda leer, agregar contenido a resultado
+		while ((linea = rd.readLine()) != null) {
+			resultado.append(linea);
+		}
+		// Cerrar el BufferedReader
+		rd.close();
+		// Regresar resultado, pero como cadena, no como StringBuilder
+		return resultado.toString();
+	}
+}
+
+*/ 
+
+    }
 }
